dc667a5511df731aaa21c2ba644e491ea940d480
anjdydhody <a01066444534@gmail.com>
2025-05-02 02:11:01 +0900

    feat: user entity 추가 #1
    - 사용자 국적 enum으로 관리

---
+++ src/main/java/com/netflix/recommend/enums/Country.java
@@ -0,0 +1,13 @@
+package com.netflix.recommend.enums;
+
+public enum Country {
+    SOUTH_KOREA("South Korea"),
+    UNITED_STATES("United States"),
+    GHANA("Ghana"),
+    BURKINA_FASO("Burkina Faso"),
+    UNITED_KINGDOM("United Kingdom");
+
+    // TODO: 추후에 추가
+
+    Country(String name) {}
+}
