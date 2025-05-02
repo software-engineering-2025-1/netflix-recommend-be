aafdcfcd39622fd394b65a18a04f30b06055f09e
anjdydhody <a01066444534@gmail.com>
2025-05-02 15:14:13 +0900

    feat: UserDetailReqDto 추가 #2

---
+++ src/main/java/com/netflix/recommend/dto/UserDetailReqDto.java
@@ -0,0 +1,15 @@
+package com.netflix.recommend.dto;
+
+import com.netflix.recommend.enums.Country;
+import com.netflix.recommend.enums.Genre;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class UserDetailReqDto {
+    private String name;
+    private Integer age;
+    private Country country;
+    private Set<Genre> genres;
+}
