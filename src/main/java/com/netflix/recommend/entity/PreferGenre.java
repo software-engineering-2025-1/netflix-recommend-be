09fbfd73d67c58901f36f44a9bf571bae50cd032
anjdydhody <a01066444534@gmail.com>
2025-05-02 15:13:37 +0900

    feat: PreferGenre entity 추가 #2
    - genre enum 추가
    - PreferGenre entity 추가

---
+++ src/main/java/com/netflix/recommend/entity/PreferGenre.java
@@ -0,0 +1,27 @@
+package com.netflix.recommend.entity;
+
+import com.netflix.recommend.enums.Genre;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "prefer-genre")
+@Table(uniqueConstraints={ @UniqueConstraint(name= "genre-user-uk", columnNames={"genre", "user_id"})})
+@NoArgsConstructor
+public class PreferGenre {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private Genre genre;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    PreferGenre(Genre genre, User user) {
+        this.genre = genre;
+        this.user = user;
+    }
+}
